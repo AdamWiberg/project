d9315d45401545949791a1ec6f43fc28d396cd6f
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-09 17:51:18 +0300

    36 раздел, Конструкция switch-case в JavaScript

--- loop.js
+++ loop.js
@@ -1486,9 +1486,30 @@
 
 
 
+// 36 раздел Конструкция switch-case в JavaScript
 
 
 
+// let num = 5;
+
+// switch (num) {
+// case 1:
+//     console.log('Зима');
+//     break;
+// case 2: 
+// console.log('Весна');
+//     break;
+//     case 3: 
+//     console.log('Лето');
+//     break;
+//     case 4: 
+//     console.log('Осень');
+//     break;
+//     default:
+//         console.log('Введите значение от 1 до 4');
+//         break;
+
+// }
 
 
 
