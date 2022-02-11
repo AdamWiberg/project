c325e82f16d67d453b86bfcb9c5802c6311bb209
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-11 23:08:25 +0300

    52 раздел, работа с флагами

---
+++ flag/flag.js
@@ -0,0 +1,19 @@
+// 52 раздел, работа с флагами
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// let flag = false;
+// let result = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   result = arr[i];
+//   if (result == 'c') {
+//       flag = true;
+//       break;
+//   }
+// }
+
+// if (flag === true) {
+//     console.log('Значение в массиве есть ' + result);
+
+// } else {
+//     console.log('В массиве нет этого значения');
+// }
