6ab652be07072f86f203ec20fc8338f3cad40a22
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-12 23:25:50 +0300

    преобразование числе в строку

---
+++ String/string.js
@@ -0,0 +1,39 @@
+// let str = '';
+
+// for (let i = 0; i < 5; i++) {
+//     str += 0;
+// }
+
+// console.log(str);
+
+
+
+
+// let num = '';
+
+// for (let i = 1; i <= 9; i++) {
+//     num += i;
+// }
+
+// console.log(num)
+
+
+
+
+// let num = '';
+
+// for (let i = 9; i >= 1; i-- ) {
+//     num += i;
+// }
+
+// console.log(num)
+
+
+
+let num = '';
+
+for (let i = 1; i <= 9; i++) {
+    num += i + '-';
+}
+
+console.log(num);
