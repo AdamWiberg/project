05fac86bbcc475c1d83aa3e2c5d4c94527a4603c
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-13 15:14:14 +0300

    57 раздел, Вложенные циклы в JavaScript

---
+++ cycle/loops/loops.js
@@ -0,0 +1,17 @@
+
+
+// for(let i = 1; i <= 9; i++) {
+//     for(let j = 1; j <= 4; j++) {
+//         document.write(i)
+//     }
+// }
+
+
+// for(let i = 1; i <= 3; i++) {
+//     for(let j = 1; j <= 3; j++ ) {
+//         for(let k = 1; k <= 3; k++ ) {
+//             document.write(String(i) + String(j) + String(k) + ' ' );
+//         }
+//     }
+// }
+
