dfd5b0f4b75519021f5503ca3e78a0d23ec10fa1
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-13 20:16:56 +0300

    59 раздел Заполнение массивов через цикл в JavaScript

--- cycle/loops/loops.js
+++ cycle/loops/loops.js
@@ -15,3 +15,51 @@
 //     }
 // }
 
+
+
+//let arr = [1,2,3, 4];
+
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3;
+// arr[3] = 4;
+// arr[4] = 5;
+// arr[5] = 6;
+// arr[6] = 7;
+// arr[7] = 8;
+// arr[8] = 9;
+// arr[9] = 10;
+
+//console.log(arr)
+
+// arr = []
+// arr[0] = 'x'
+// arr[1] = 'x'
+// arr[2] = 'x'
+// arr[3] = 'x'
+// arr[4] = 'x' 
+// console.log(arr);
+
+
+
+
+
+
+
+
+// let arr = [];
+
+// for(let i = 0; i <= 100; i = i + 1) {
+//     arr[i] = i + 1;
+// }
+
+// console.log(arr);
+
+
+// let arr = [];
+
+// for (let i = 1, j = 0; i <= 99; i += 2, j++) {
+//     arr[j] = i
+// }
+
+// console.log(arr) //нечетные числа от 1 до 99
