63a2effd6dfe6d6d4859c472a2a493999047b293
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-13 15:02:27 +0300

    56 раздел Цикл for и типы данных JavaScript

---
+++ cycle/for/for.js
@@ -0,0 +1,16 @@
+
+
+// for (let i = 10; i <= 1000; i++) {
+// let str = String(i);
+// if (Number(str[0]) + Number(str[1]) == 5) {
+//     console.log(i)
+// }
+// } // сумма двух первых чисел равна 5
+
+
+// let result = 0;
+// for (let i = 1; i <= 30; i++) {
+//  result += i;
+// }
+
+// console.log(result);
