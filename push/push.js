13bb3cca8b002c80a951b34fda3d43023feff549
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-14 13:31:12 +0300

    61 раздел  Заполнение массива методом push в JavaScript

---
+++ push/push.js
@@ -0,0 +1,178 @@
+// 61 раздел  Заполнение массива методом push в JavaScript
+
+
+// let arr = [];
+
+// arr.push(1);
+// arr.push(2);
+// arr.push(3);
+// arr.push(4);
+// arr.push(5);
+// arr.push(6);
+// arr.push(7);
+// arr.push(8);
+// arr.push(9);
+// arr.push(10);
+// arr.push(11);
+// arr.push(12);
+// arr.push(13);
+
+// console.log(arr);
+
+
+
+
+
+
+// let str = 'x';
+
+// for (let i = 1; i <= 10; i++) {
+//     arr.push('x');
+// }
+
+// console.log(arr);
+
+
+
+
+
+// let arr = [-2, -5, 6, -9, 4, -3, 4, -7];
+// let result = [];
+// let val = 0;
+// // for(let elem of arr) {
+// // if (elem > 0) {
+// //     result.push(elem)
+// // }
+// // }
+// for (let i = 0; i < arr.length; i++) {
+//       val = arr[i];
+//     if (val > 0) {
+//         result.push(val)
+// }
+// }
+// console.log(result)
+
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// let result = [];
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     result.push(arr[i]);
+// }
+// console.log(result); // переворот массива
+
+
+
+
+// let arr = [];
+
+// arr.push('a');
+// arr.push('b');
+// arr.push('c');
+
+// console.log(arr);
+
+
+
+// let arr = [];
+
+// for (let i = 1; i <= 5; i++) {
+//     arr.push(i);
+// }
+//  console.log(arr);
+
+
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let result = [];
+for(let elem of arr) {
+    if(elem % 2 != 0) { // нечетные числа
+        result.push(elem);
+    }
+}
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
