dc8737d067dc02ce091955e5babc84394cd73a30
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-15 15:49:47 +0300

    67 раздел Вывод пирамидок на JavaScript

---
+++ piramid/piramid.js
@@ -0,0 +1,13 @@
+//67 раздел Вывод пирамидок на JavaScript
+
+
+
+// for (let i = 1; i <= 9; i += 2) {
+//     let str = '';
+
+//     for (let j = 0; j < i; j++) {
+//         str += i;
+        
+//     }
+//     document.write(str + '<br>');
+// } 
