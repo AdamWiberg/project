3faf476b161dc304cdf4df97ffb84f79c70df19f
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-15 17:17:21 +0300

    67 раздел Вывод пирамидок на JavaScript

--- piramid/piramid.js
+++ piramid/piramid.js
@@ -11,3 +11,29 @@
 //     }
 //     document.write(str + '<br>');
 // } 
+
+// for(let i = 10; i >= 1; i -= 2) {
+//     let str = '';
+
+//     for(let j = 0; j < i; j++) {
+//         str += 'x';
+//     }
+
+//     document.write(str + '<br>');
+// } //обратный циклс
+
+
+
+for (let i = 9; i >= 1; i--) {
+    let str = '';
+
+    for (let j = 0; j < i; j++) {
+        str += i;
+    }
+    document.write(str + '<br>'); 
+}
+
+
+
+
+
