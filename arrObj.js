a7078666e429e38549c9d1ffd76a1de43e9e7343
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-17 16:36:01 +0300

    76 раздел Многомерные массивы и объекты в JavaScript

---
+++ arrObj.js
@@ -0,0 +1,37 @@
+// 76 раздел Многомерные массивы и объекты в JavaScript
+
+
+
+let employees = [
+    {
+        name: 'name1',
+        salary: 300,
+    },
+    {
+        name: 'name2',
+        salary: 400,
+    },
+    {
+        name: 'name3',
+        salary: 500,
+    },
+];
+
+
+
+console.log(employees[0].salary);
+console.log(employees[1].salary);
+
+
+
+
+
+
+
+let students = {
+    'boys': ['Коля', 'Вася', 'Петя'],
+    'girls': ['Даша', 'Маша', 'Лена'],
+};
+
+console.log(students['boys'][1]);
+console.log(students['girls'][2]);
