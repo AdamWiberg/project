f3bdb32621f797612b96862360532c8622c333c9
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-22 22:40:44 +0300

    Логические операторы без if в функциях JavaScript

---
+++ function/if.js
@@ -0,0 +1,37 @@
+// Логические операторы без if в функциях JavaScript
+
+
+function func (a, b) {
+    return a == b
+}
+
+console.log(func(1, 2))
+
+
+
+function func (a, b) {
+    return a != b;
+}
+
+console.log(func(1, 1))
+
+
+
+
+
+function func(a, b) {
+    return a + b >= 10;
+}
+
+console.log(func(1, 11))
+
+
+
+
+
+
+function func(num) {
+    return num >= 0
+}
+
+console.log(func(-2))
