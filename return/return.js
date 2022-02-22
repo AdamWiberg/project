a39da73c199795fa8238edda3438c9f7a188395b
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-02-22 13:14:43 +0300

    84 Инструкция return в JavaScript

---
+++ return/return.js
@@ -0,0 +1,189 @@
+// 84 Инструкция return в JavaScript
+
+
+
+
+// function cube(num) {
+//     return Math.pow(num, 3);
+// }
+
+// let result = cube(3);
+
+// alert(result)
+
+
+
+
+
+// function sqrt(num) {
+//     return Math.sqrt(num)
+// }
+ 
+// let sqrt3 = sqrt(3);
+// let sqrt4 = sqrt(4);
+
+// let sum = sqrt3 + sqrt4;
+
+// console.log(sum)
+
+
+
+
+
+// function sqrt(num) {
+//     return Math.sqrt(num);
+// }
+
+// function round(num) {
+//     return num.toFixed(3)
+// }
+
+// let square = round(sqrt(2));
+
+// console.log(square)
+
+
+
+
+// function sqrt(num) {
+//     return Math.sqrt(num);
+// }
+
+// function sum(num1, num2, num3) {
+//     return num1 + num2 + num3;
+// }
+
+// function round(num) {
+//     return num.toFixed(3);
+// }
+
+
+//  let sqrt2 = sqrt(2);
+//  let sqrt3 = sqrt(3);
+//  let sqrt4 = sqrt(4);
+
+//  let result =  round(sum(sqrt2, sqrt3, sqrt4));
+
+//  console.log(result)
+
+
+
+
+
+
+// function func(num) {
+// 	return num;
+	
+// 	let result = num * num;
+// 	return result;
+// }
+
+// alert( func(3) );
+
+
+
+
+
+
+
+
+
+
+
+// function func(num) {
+// 	if (num <= 0) {
+// 		return Math.abs(num);
+// 	} else {
+// 		return num * num;
+// 	}
+// }
+
+// alert( func(10) );
+// alert( func(-5) );
+
+
+
+
+
+
+
+// function func(num) {
+// 	if (num <= 0) {
+// 		return Math.abs(num);
+// 	}
+	
+// 	return num * num;
+// }
+
+// alert( func(10) );
+// alert( func(-5) );
+
+
+
+
+
+
+// function func(num) {
+// 	let sum = 0;
+	
+// 	for (let i = 1; i <= num; i++) {
+// 		sum += i;
+		
+// 	}
+//     return sum;
+// }
+
+// alert( func(5) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function func(arr) {
+//     let sum = 1;
+//     while(true) {
+//         arr /= 2;
+
+//         if (arr < 10) {
+//             return sum;
+//         }
+//         sum++;
+//     }
+// }
+
+
+// let result = func([109]);
+
+// console.log(result); // 4
+
+
+
+
+// function sum(num1, num2) {
+//     if (num1 > 0 && num2 > 0) {
+//     return num1 * num2;
+// } else {
+//     return num1 - num2;
+// }
+// }
+
+
+
+// console.log(sum(3, 2))
+
+
+
+
+
+
+
