b28958d95f60c009cc63bb5a38ac114df4deb953
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-05-12 22:55:38 +0300

    Правильное использование функций JavaScript

--- function/JavaScriptFunctions.js
+++ function/JavaScriptFunctions.js
@@ -3,78 +3,78 @@
 // Задача №1
 
 
-// function getDigitsSum(num) {
-//     let sum = 0;
-//     let digits = String(num).split('');
+function getDigitsSum(num) {
+    let sum = 0;
+    let digits = String(num).split('');
 
-//     for (let digit of digits) {
-//         sum += Number(digit);
-//     }
-//     return sum;
-// }
+    for (let digit of digits) {
+        sum += Number(digit);
+    }
+    return sum;
+}
 
-// getDigitsSum()
+getDigitsSum()
 
 
 
-// let result = [];
-// function getDigitsSums() {
-//     for (let i = 1; i <= 2030; i++) {
-//         if(getDigitsSum(i) == 13) {
-//             result.push(i)
-//         }
-//     }
-// }
-// getDigitsSums()
-// console.log(result) // сумма цифр которых равна 13.
+let result = [];
+function getDigitsSums() {
+    for (let i = 1; i <= 2030; i++) {
+        if(getDigitsSum(i) == 13) {
+            result.push(i)
+        }
+    }
+}
+getDigitsSums()
+console.log(result) // сумма цифр которых равна 13.
 
 
 //Задача №2
 
 
-// function isPrime(num) {
-//     for(let i = 2; i < num; i++) {
-//         if(num % i == 0) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
+function isPrime(num) {
+    for(let i = 2; i < num; i++) {
+        if(num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-// isPrime();
+isPrime();
 
-// let result = []
-// function primeNumbers () {
-//     for (let i = 1; i <= 100; i++) {
-//         if(isPrime(i)) {
-//             result.push(i)
-//         }
-//     }
-// }
-// primeNumbers()
-// console.log(result); // все простые числа в интервале от 1 до 100.
+let result = []
+function primeNumbers () {
+    for (let i = 1; i <= 100; i++) {
+        if(isPrime(i)) {
+            result.push(i)
+        }
+    }
+}
+primeNumbers()
+console.log(result); // все простые числа в интервале от 1 до 100.
 
 
 //Задача № 3
 
 
 
-// function reverseNum(num) {
-//     return String(num).split('').reverse().join('');
-// }
+function reverseNum(num) {
+    return String(num).split('').reverse().join('');
+}
 
 
 
 
 
-// let arr = [123, 456, 789];
-// let result = [];
-// for(let num of arr) {
-//         result.push(reverseNum(num));
-// }
+let arr = [123, 456, 789];
+let result = [];
+for(let num of arr) {
+        result.push(reverseNum(num));
+}
 
 
-// console.log(result) // переворот числа выполняла отдельная функция.
+console.log(result) // переворот числа выполняла отдельная функция.
 
 
 
@@ -82,21 +82,21 @@
 
 
 
-// function getDigitsSum (num) {
-//     let digits = String(num).split('')
-//     let sum = 0;
-//     for(let digit of digits) {
-//         sum += Number(digit);
-//     }
-//     return sum;
-// }
+function getDigitsSum (num) {
+    let digits = String(num).split('')
+    let sum = 0;
+    for(let digit of digits) {
+        sum += Number(digit);
+    }
+    return sum;
+}
 
-// function getDigitsSums (fromNum, toNum) {
-//     if(getDigitsSum(fromNum) === getDigitsSum(toNum)) {
-//         console.log('Числа совпадают')
-//     }else {
-//         console.log('Числа не совпадают')
-//     }
-// }
+function getDigitsSums (fromNum, toNum) {
+    if(getDigitsSum(fromNum) === getDigitsSum(toNum)) {
+        console.log('Числа совпадают')
+    }else {
+        console.log('Числа не совпадают')
+    }
+}
 
-// getDigitsSums(236, 533) // равна ли сумма цифр первого числа сумме цифр второго числа.
+getDigitsSums(236, 533) // равна ли сумма цифр первого числа сумме цифр второго числа.
