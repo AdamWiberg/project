4754b6f7b7c6342619f427feb8d7a1be997da856
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-05-17 20:05:06 +0300

    Function expression и Function declaration в JavaScript

---
+++ function/expressiondeclaration.js
@@ -0,0 +1,33 @@
+//Function expression и Function declaration в JavaScript
+
+// Задача №1
+
+// Function declaration
+
+func()
+
+function func () {
+    console.log('test');
+}
+
+
+
+// Function expreccion
+func()
+let func = function() {
+    console.log('бер')
+}
+
+
+//Расставьте точки с запятой во всех необходимых местах:
+
+let func1 = function() {
+    alert('!')
+};
+let func2 = function() {
+	alert('!')
+};
+
+function func3() {
+	alert('!')
+}
