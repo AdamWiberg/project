28446804bd259f3b651a4244691197bedf3a7082
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-09-08 15:16:23 +0300

    JS

---
+++ my-app/src/Test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <a href = '#'>Меню</a>
+            <a href = '#'>Экспорт</a>
+            <a href = '#'>Вконтакте</a>
+
+        </div>
+        
+    );
+}
+
+export default Test;
